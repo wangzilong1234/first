aafac3fb1a1ed04ed70c44fb1ba46f491c4a674d
wangzilong <734987891@qq.com>
2020-04-01 16:06:37 +0800

    add timedll

---
+++ Assets/test2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnrealM;
+
+public class test2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Log.Info(Time.realtimeSinceStartup.ToString());
+        this.Delayer(5,() => {
+            Log.Info("{0}:{1}_____","你好","CODE");
+            Log.Info(Time.realtimeSinceStartup.ToString());
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
