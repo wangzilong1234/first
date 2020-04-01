66d6ec37a316ed8b4f36a213813e260b3454fd6d
wangzilong <734987891@qq.com>
2020-04-01 16:29:48 +0800

    Update test2.cs
    test test test

--- Assets/test2.cs
+++ Assets/test2.cs
@@ -5,6 +5,15 @@ using UnrealM;
 
 public class test2 : MonoBehaviour
 {
+    private void Awake() {
+        MessengerCenter.EventCenter.AddListener(1,(data) => {
+            foreach (var item in data) {
+                Log.Info(item.ToString());
+            }
+
+        });
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,21 @@ public class test2 : MonoBehaviour
             Log.Info("{0}:{1}_____","你好","CODE");
             Log.Info(Time.realtimeSinceStartup.ToString());
         });
+
+        MessengerCenter.EventCenter.Broadcast(1,1111,2222,3333);
+    }
+
+    private void OnEnable() {
+       
+    }
+
+    private void OnDestroy() {
+        MessengerCenter.EventCenter.RemoveListener(1,(data) => {
+            foreach (var item in data) {
+                Log.Info(item.ToString());
+            }
+        });
+        Log.Info("remove");
     }
 
     // Update is called once per frame
