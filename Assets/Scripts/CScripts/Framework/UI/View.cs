3dcda5144119ec4ea2da8ba0005ee9b76fd25193
wangzilong <734987891@qq.com>
2020-04-07 18:41:30 +0800

    1111
    commit

---
+++ Assets/Scripts/CScripts/Framework/UI/View.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ViewShowAnimator {
+    public static void PlayAnimator(Animator animator,string clipName,AnimatorFinishEventTrigger eventTrigger,System.Action callback) {
+        if (animator!=null) {
+            animator.Play(clipName,-1,0);
+        }
+        if (eventTrigger!=null) {
+            eventTrigger.OnFinishAnimation=aniName => {
+                if (callback!=null&&aniName==clipName) {
+                    callback();
+                }
+            };
+        }
+    }
+}
+
+public class View : MonoBehaviour,IView
+{
+    public Animator animator;
+    public AnimatorFinishEventTrigger animatorTrigger;
+    public string showAnimName = "Open";
+    public string hideAnimName = "Close";
+    public event System.Action onStart;
+    public event System.Action onClose;
+    public virtual void OnStart() {}
+    public virtual void OnClose() {}
+    void Start()
+    {
+        OnStart();
+        onStart?.Invoke();
+    }
+
+    private void OnDestroy() {
+        OnClose();
+        onClose?.Invoke();
+    }
+
+    IEnumerator PlayAnimation() {
+        if (animator!=null&&animatorTrigger!=null) {
+            ViewShowAnimator.PlayAnimator(animator,showAnimName,animatorTrigger,() => {
+                
+            });
+        }
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/CScripts/XLuaManager.cs
@@ -0,0 +1,201 @@
+﻿using System.IO;
+using UnityEngine;
+using XLua;
+
+[Hotfix]
+[LuaCallCSharp]
+public class XLuaManager : MonoSingleton<XLuaManager>
+{
+    public const string luaAssetbundleAssetName = "Lua";
+    public const string luaScriptsFolder = "LuaScripts";
+    const string commonMainScriptName = "Common.Main";
+    const string gameMainScriptName = "GameMain";
+    const string hotfixMainScriptName = "XLua.HotfixMain";
+    LuaEnv luaEnv = null;
+    LuaUpdater luaUpdater = null;
+
+    protected override void Init()
+    {
+        base.Init();     
+        InitLuaEnv();
+    }
+
+    public bool HasGameStart
+    {
+        get;
+        protected set;
+    }
+
+    public LuaEnv GetLuaEnv()
+    {
+        return luaEnv;
+    }
+
+    void InitLuaEnv()
+    {
+        luaEnv = new LuaEnv();
+        HasGameStart = false;
+        if (luaEnv != null)
+        {
+            luaEnv.AddLoader(CustomLoader);
+          
+        }
+        else
+        {
+           
+        }
+    }
+
+    public void OnInit()
+    {
+        if (luaEnv != null)
+        {
+            LoadScript(commonMainScriptName);
+            luaUpdater = gameObject.GetComponent<LuaUpdater>();
+            if (luaUpdater == null)
+            {
+                luaUpdater = gameObject.AddComponent<LuaUpdater>();
+            }
+            luaUpdater.OnInit(luaEnv);
+        }
+    }
+
+    public string AssetbundleName
+    {
+        get;
+        protected set;
+    }
+
+    // 重启虚拟机：热更资源以后被加载的lua脚本可能已经过时，需要重新加载
+    // 最简单和安全的方式是另外创建一个虚拟器，所有东西一概重启
+    public void Restart()
+    {
+        StopHotfix();
+        Dispose();
+        InitLuaEnv();
+        OnInit();
+    }
+
+    public void SafeDoString(string scriptContent)
+    {
+        if (luaEnv != null)
+        {
+            try
+            {
+                luaEnv.DoString(scriptContent);
+            }
+            catch (System.Exception ex)
+            {
+                string msg = string.Format("xLua exception : {0}\n {1}", ex.Message, ex.StackTrace);
+                Log.Error(msg, null);
+            }
+        }
+    }
+
+    public void StartHotfix(bool restart = false)
+    {
+        if (luaEnv == null)
+        {
+            return;
+        }
+
+        if (restart)
+        {
+            StopHotfix();
+            ReloadScript(hotfixMainScriptName);
+        }
+        else
+        {
+            LoadScript(hotfixMainScriptName);
+        }
+        SafeDoString("HotfixMain.Start()");
+    }
+
+    public void StopHotfix()
+    {
+        SafeDoString("HotfixMain.Stop()");
+    }
+
+    public void StartGame()
+    {
+        if (luaEnv != null)
+        {
+            LoadScript(gameMainScriptName);
+            SafeDoString("GameMain.Start()");
+            HasGameStart = true;
+        }
+    }
+    
+    public void ReloadScript(string scriptName)
+    {
+        SafeDoString(string.Format("package.loaded['{0}'] = nil", scriptName));
+        LoadScript(scriptName);
+    }
+
+    void LoadScript(string scriptName)
+    {
+        SafeDoString(string.Format("require('{0}')", scriptName));
+    }
+
+    public static byte[] CustomLoader(ref string filepath)
+    {
+        string scriptPath = string.Empty;
+        filepath = filepath.Replace(".", "/") + ".lua";
+#if UNITY_EDITOR     
+            scriptPath = Path.Combine(Application.dataPath, luaScriptsFolder);
+            scriptPath = Path.Combine(scriptPath, filepath);
+            Log.Info("Load lua script : " + scriptPath);
+            return GameUtility.SafeReadAllBytes(scriptPath);
+       
+#endif     
+    }
+
+    private void Update()
+    {
+        if (luaEnv != null)
+        {
+            luaEnv.Tick();
+
+            if (Time.frameCount % 100 == 0)
+            {
+                luaEnv.FullGc();
+            }
+        }
+    }
+
+    private void OnLevelWasLoaded()
+    {
+        if (luaEnv != null && HasGameStart)
+        {
+            SafeDoString("GameMain.OnLevelWasLoaded()");
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (luaEnv != null && HasGameStart)
+        {
+            SafeDoString("GameMain.OnApplicationQuit()");
+        }
+    }
+
+    public override void Dispose()
+    {
+        if (luaUpdater != null)
+        {
+            luaUpdater.OnDispose();
+        }
+        if (luaEnv != null)
+        {
+            try
+            {
+                luaEnv.Dispose();
+                luaEnv = null;
+            }
+            catch (System.Exception ex)
+            {
+                string msg = string.Format("xLua exception : {0}\n {1}", ex.Message, ex.StackTrace);                
+            }
+        }
+    }
+}
