e718540b56c8edd52b568f5cf1920d0056e58670
wangzilong <734987891@qq.com>
2020-04-07 13:36:26 +0800

    commit
    111

--- Assets/Scripts/CScripts/MonoSingleton.cs
+++ Assets/Scripts/CScripts/MonoSingleton.cs
@@ -1,21 +1,29 @@
 using UnityEngine;
 
-public abstract class MonoSingleton<T>:MonoBehaviour where T : MonoSingleton<T> {
-    private static T mInstance = null;
-    public static T Instance {
-        get {
-            if (mInstance==null) {
-                mInstance=GameObject.FindObjectOfType(typeof(T)) as T;
-                if (mInstance==null) {
-                    GameObject go = new GameObject("___"+typeof(T).Name);
-                    mInstance=go.AddComponent<T>();
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+	private static T mInstance = null;
+
+	public static T Instance
+    {
+        get
+        {
+			if (mInstance == null)
+            {
+            	mInstance = GameObject.FindObjectOfType(typeof(T)) as T;
+                if (mInstance == null)
+                {
+                    GameObject go = new GameObject(typeof(T).Name);
+                    mInstance = go.AddComponent<T>();
                     GameObject parent = GameObject.Find("Boot");
-                    if (parent==null) {
-                        parent=new GameObject("Boot");
+                    if (parent == null)
+                    {
+                        parent = new GameObject("Boot");
                         GameObject.DontDestroyOnLoad(parent);
                     }
-                    if (parent!=null) {
-                        go.transform.parent=parent.transform;
+                    if (parent != null)
+                    {
+                        go.transform.parent = parent.transform;
                     }
                 }
             }
@@ -25,31 +33,38 @@ public abstract class MonoSingleton<T>:MonoBehaviour where T : MonoSingleton<T> 
     }
 
     /*
-     * Ã»ÓÐÈÎºÎÊµÏÖµÄº¯Êý£¬ÓÃÓÚ±£Ö¤MonoSingletonÔÚÊ¹ÓÃÇ°ÒÑ´´½¨
+     * 没有任何实现的函数，用于保证MonoSingleton在使用前已创建
      */
-    public void Startup() {
+    public void Startup()
+    {
 
     }
 
-    private void Awake() {
-        if (mInstance==null) {
-            mInstance=this as T;
+    private void Awake()
+    {
+        if (mInstance == null)
+        {
+            mInstance = this as T;
         }
+
         DontDestroyOnLoad(gameObject);
         Init();
     }
-
-    protected virtual void Init() {
+ 
+    protected virtual void Init()
+    {
 
     }
 
-    public void DestroySelf() {
+    public void DestroySelf()
+    {
         Dispose();
-        MonoSingleton<T>.mInstance=null;
+        MonoSingleton<T>.mInstance = null;
         UnityEngine.Object.Destroy(gameObject);
     }
 
-    public virtual void Dispose() {
+    public virtual void Dispose()
+    {
 
     }
 
--- Assets/Scripts/CScripts/Singleton.cs
+++ Assets/Scripts/CScripts/Singleton.cs
@@ -1,28 +1,36 @@
-﻿using System;
+using System;
 using UnityEngine;
 
-public abstract class Singleton<T> where T : class, new() {
-    private static T m_instance;
-    public static T instance {
-        get {
-            if (Singleton<T>.m_instance==null) {
-                Singleton<T>.m_instance=Activator.CreateInstance<T>();
-                if (Singleton<T>.m_instance!=null) {
+public abstract class Singleton<T> where T : class, new()
+{
+	private static T m_instance;
+	public static T instance
+	{
+		get
+        {
+            if (Singleton<T>.m_instance == null)
+		    {
+			    Singleton<T>.m_instance = Activator.CreateInstance<T>();
+			    if (Singleton<T>.m_instance != null)
+			    {
                     (Singleton<T>.m_instance as Singleton<T>).Init();
-                }
-            }
+			    }
+		    }
 
             return Singleton<T>.m_instance;
         }
-    }
+	}
 
-    public static void Release() {
-        if (Singleton<T>.m_instance!=null) {
-            Singleton<T>.m_instance=(T)((object)null);
-        }
-    }
+	public static void Release()
+	{
+		if (Singleton<T>.m_instance != null)
+		{
+			Singleton<T>.m_instance = (T)((object)null);
+		}
+	}
 
-    public virtual void Init() {
+    public virtual void Init()
+    {
 
     }
 
