965dd18957f260d39fce551d47ec441a4ae12d9d
wangzilong1234 <734987891@qq.com>
2020-04-09 20:45:11 +0800

    1111

---
+++ Assets/TestScene.cs
@@ -0,0 +1,17 @@
+﻿using Game;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        XLuaManager.Instance.StartGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
